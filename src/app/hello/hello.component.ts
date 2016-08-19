b600b67fa2d83c6304f296f383f1bb09a0b532df
Luis Arce <luis.arce@jalasoft.com>
2016-08-19 18:39:51 -0400

    Init angular 2 Application

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-hello',
+  templateUrl: 'hello.component.html',
+  styleUrls: ['hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
